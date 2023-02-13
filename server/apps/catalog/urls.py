27c38d46180da4cd3f95696198f175e943d7452d
MrEndor <mrendorer@gmail.com>
2023-02-13 18:25:39 +0300

    fix(catalog-url): добавил url со своим конвертером

--- server/apps/catalog/urls.py
+++ server/apps/catalog/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', item_list, name='item_list'),
     path('/<int:catalog_id>', item_detail, name='item_detail'),
     re_path('^re/(?P<catalog_id>[0-9]+)/$', item_detail, name='re_item_detail'),
+    path(
+        'converter/<own_int:catalog_id>',
+        item_detail,
+        name='convert_item_detail',
+    ),
 ]
