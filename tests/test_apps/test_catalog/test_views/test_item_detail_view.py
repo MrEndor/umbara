75fdc842d570b5f06175eb1edc33002983cf626d
MrEndor <mrendorer@gmail.com>
2023-02-14 21:37:02 +0300

    Revert "test(tests): добавил генерацию данных"
    This reverts commit 834a9b7f7c58536a93d7295e52be0bcc4c011d26.
    
    	modified:   tests/test_apps/test_catalog/test_views/test_item_detail_view.py

--- tests/test_apps/test_catalog/test_views/test_item_detail_view.py
+++ tests/test_apps/test_catalog/test_views/test_item_detail_view.py
@@ -2,40 +2,11 @@ from http import HTTPStatus
 
 from django.test.client import Client
 from django.urls import reverse
-from hypothesis import given, strategies
-
-NegativeNumbers = strategies.integers(max_value=-1)
-PositiveNumbers = strategies.integers(min_value=0)
 
 
-@given(catalog_id=PositiveNumbers)
-def test_item_detail_page(
-    client: Client,
-    catalog_item_detail_body: str,
-    catalog_id: int,
-):
+def test_item_detail_page(client: Client, catalog_item_detail_body):
     """This test ensures that item detail page works."""
-    response = client.get(reverse('catalog:item_detail', args=(catalog_id,)))
+    response = client.get(reverse('catalog:item_detail', args=(1, )))
 
     assert response.status_code == HTTPStatus.OK
     assert response.content.decode() == catalog_item_detail_body
-
-
-@given(catalog_id=NegativeNumbers)
-def test_fail_statement_regex_url_item_detail(client: Client, catalog_id: int):
-    """This test ensures that url path works."""
-    response = client.get(
-        '/catalog/re/{catalog_id}/'.format(catalog_id=catalog_id),
-    )
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-
-
-@given(catalog_id=strategies.from_regex('[0-9]+', fullmatch=True))
-def test_true_statement_regex_url_item_detail(client: Client, catalog_id: str):
-    """This test ensures that url path works."""
-    response = client.get(
-        '/catalog/re/{catalog_id}/'.format(catalog_id=catalog_id),
-    )
-
-    assert response.status_code == HTTPStatus.OK
