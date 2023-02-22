a68e6541d4d93c687b0c056925f963cf2a754cba
MrEndor <mrendorer@gmail.com>
2023-02-22 14:35:24 +0300

    fix(catalog): fix django warning

--- server/apps/catalog/urls.py
+++ server/apps/catalog/urls.py
@@ -6,7 +6,7 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', item_list, name='item_list'),
-    path('/<int:catalog_id>', item_detail, name='item_detail'),
+    path('<int:catalog_id>', item_detail, name='item_detail'),
     re_path('^re/(?P<catalog_id>[0-9]+)/$', item_detail, name='re_item_detail'),
     path(
         'converter/<own_int:catalog_id>',
