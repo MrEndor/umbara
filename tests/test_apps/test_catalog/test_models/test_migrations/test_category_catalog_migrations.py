fe2776df1e88a88e611c5c55f6c14f90b0c3783d
MrEndor <mrendorer@gmail.com>
2023-02-22 14:44:10 +0300

    feat(test): добавил тесты для миграции catalog

---
+++ tests/test_apps/test_catalog/test_models/test_migrations/test_category_catalog_migrations.py
@@ -0,0 +1,20 @@
+import pytest
+from django_test_migrations.migrator import Migrator
+
+from server.apps.catalog.urls import app_name
+
+
+def test_initial0001(migrator: Migrator) -> None:
+    """Tests the initial migration forward application."""
+    old_state = migrator.apply_initial_migration((app_name, None))
+    with pytest.raises(LookupError):
+        old_state.apps.get_model(app_name, 'CatalogCategory')
+
+    new_state = migrator.apply_tested_migration((app_name, '0001_initial'))
+    model = new_state.apps.get_model(app_name, 'CatalogCategory')
+
+    assert model.objects.create(
+        name='qwerty',
+        weight=20000,
+        slug='asd290as',
+    )
