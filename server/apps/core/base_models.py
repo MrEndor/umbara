9ce0fbaf006cfeadefafdda9fbe624a21157be38
MrEndor <mrendorer@gmail.com>
2023-02-23 20:45:10 +0300

    feat(core): добавил NormalizedName model

--- server/apps/core/base_models.py
+++ server/apps/core/base_models.py
@@ -1,7 +1,11 @@
 from django.core import validators
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.text import format_lazy
+from django.utils.translation import gettext_lazy as _
 
 from server.apps.core import constances
+from server.apps.core.normalize import normalize
 
 
 class Identifiable(models.Model):
@@ -27,8 +31,9 @@ class Nameable(models.Model):
 
     name = models.CharField(
         max_length=constances.MAX_NAME_LENGTH,
-        verbose_name='Название',
-        help_text='Максимальная длина {maximum}'.format(
+        verbose_name=_('Name'),
+        help_text=format_lazy(
+            _('Maximum length {maximum}'),
             maximum=constances.MAX_NAME_LENGTH,
         ),
     )
@@ -43,7 +48,7 @@ class Publishable(models.Model):
 
     is_published = models.BooleanField(
         default=True,
-        verbose_name='Опубликовано',
+        verbose_name=_('Published'),
     )
 
     class Meta:
@@ -65,3 +70,36 @@ class Slugable(models.Model):
     class Meta:
         verbose_name = 'Slugable'
         abstract = True
+
+
+class NormalizedName(Nameable):
+    """Abstract model for normalized name."""
+
+    normalized_name = models.CharField(  # noqa: DJ01
+        max_length=constances.MAX_NAME_LENGTH,
+        editable=False,
+        unique=True,
+        null=True,
+        verbose_name=_('Normalized Name'),
+    )
+
+    class Meta:
+        verbose_name = 'Normalized name'
+        abstract = True
+
+    def clean(self) -> None:
+        """Validate model."""
+        if self.normalized_name:
+            return
+
+        name = normalize(self.name)
+
+        if self.__class__.objects.filter(normalized_name=name).exists():
+            raise ValidationError({'name': _('Name is already exists')})
+
+    def save(
+        self, *args, **kwargs,
+    ) -> None:
+        """Save model."""
+        self.normalized_name = normalize(self.name)
+        super().save(*args, **kwargs)
