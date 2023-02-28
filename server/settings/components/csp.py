90656d705a2ad7dff1aed1083cd0051900f96bac
MrEndor <mrendorer@gmail.com>
2023-02-28 18:13:06 +0300

    feat(csp): добавил unsafe-inline для boostrap

--- server/settings/components/csp.py
+++ server/settings/components/csp.py
@@ -9,7 +9,8 @@ Docs: https://github.com/mozilla/django-csp
 """
 
 CSP_SCRIPT_SRC = ("'self'",)
+CSP_SCRIPT_SRC_ELEM = ("'self'", "'unsafe-inline'", 'ajax.googleapis.com', 'cdn.jsdelivr.net')
 CSP_IMG_SRC = ("'self'",)
 CSP_FONT_SRC = ("'self'",)
-CSP_STYLE_SRC = ("'self'",)
+CSP_STYLE_SRC = ("'self'", "'unsafe-inline'", 'cdn.jsdelivr.net')
 CSP_DEFAULT_SRC = ("'none'",)
