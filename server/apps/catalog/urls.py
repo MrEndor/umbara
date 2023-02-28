693e2a791fb2cb979b87bcf6e9412b292d374981
MrEndor <mrendorer@gmail.com>
2023-03-01 00:07:08 +0300

    fix(catalog-urls): fix parameter

--- server/apps/catalog/urls.py
+++ server/apps/catalog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('<int:product_id>', item_detail, name='item_detail'),
     re_path('^re/(?P<product_id>[0-9]+)/$', item_detail, name='re_item_detail'),
     path(
-        'converter/<own_int:catalog_id>',
+        'converter/<own_int:product_id>',
         item_detail,
         name='convert_item_detail',
     ),
