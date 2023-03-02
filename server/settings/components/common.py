f8d5a039f9f3b136704bba3b4c55592ea671ea2d
MrEndor <mrendorer@gmail.com>
2023-03-02 21:22:59 +0300

    feat(conflict): resolve conflict

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -47,13 +47,8 @@ INSTALLED_APPS: Tuple[str, ...] = (
     # sorl-thumbnail
     'sorl.thumbnail',
 
-<<<<<<< HEAD
-    # bootstrap5
-    'bootstrap5',
-=======
     # ckeditor
     'ckeditor',
->>>>>>> 157408e (feat(bootstrap5): замена удалённой зависимостью локальной)
 
     # Security:
     'axes',
@@ -229,13 +224,6 @@ EMAIL_TIMEOUT = 5
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 
-<<<<<<< HEAD
-BOOTSTRAP5 = {
-    'error_css_class': 'bootstrap5-error',
-    'required_css_class': 'bootstrap5-required',
-    'javascript_in_head': True,
-}
-=======
 # Ckeditor
 CKEDITOR_CONFIGS = {
     'default': {
@@ -261,4 +249,3 @@ CKEDITOR_CONFIGS = {
 }
 
 CKEDITOR_UPLOAD_PATH = MEDIA_ROOT / 'uploads'
->>>>>>> 157408e (feat(bootstrap5): замена удалённой зависимостью локальной)
