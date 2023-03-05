3c8e8dd4731781a994d818a51496c4baf7982ef6
MrEndor <mrendorer@gmail.com>
2023-03-05 19:12:24 +0300

    feat(homepage): поменял homepage view

--- server/apps/homepage/views.py
+++ server/apps/homepage/views.py
@@ -1,9 +1,27 @@
-from django.http import HttpRequest, HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from server.apps.catalog.logic.queries import item
 
 
 def home(request: HttpRequest) -> HttpResponse:
     """View for project homepage."""
+    products = item.all_on_main()
+
+    page_number = request.GET.get('page', default=1)
+
+    paginator = Paginator(products, per_page=5)
+
+    try:
+        page_obj = paginator.page(page_number)
+    except (PageNotAnInteger, EmptyPage):
+        raise Http404('No such page exists.')
+
+    context = {
+        'product_page': page_obj,
+    }
+
     return render(
-        request, 'homepage/index.html',
+        request, 'homepage/index.html', context,
     )
