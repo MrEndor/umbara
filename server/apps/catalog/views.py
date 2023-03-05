13e62a0e858a46f84eed7b6ee93d13789b5f89f4
MrEndor <mrendorer@gmail.com>
2023-03-05 19:11:39 +0300

    feat(catalog): поменял views

--- server/apps/catalog/views.py
+++ server/apps/catalog/views.py
@@ -1,24 +1,21 @@
-from typing import Final
-
-from django.core.paginator import Paginator
-from django.http import HttpRequest, HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from server.apps.catalog.models import CatalogItem
-
-ITEM_LIST_TEXT: Final[str] = 'Список элементов'
-ITEM_DETIAL_TEXT: Final[str] = 'Подробно элемент'
+from server.apps.catalog.logic.queries import item
 
 
 def item_list(request: HttpRequest) -> HttpResponse:
     """View for the item list."""
-    products = CatalogItem.objects.exclude(
-        is_published=False,
-    ).all()
+    products = item.list_products()
     page_number = request.GET.get('page', default=1)
 
     paginator = Paginator(products, per_page=5)
-    page_obj = paginator.page(page_number)
+
+    try:
+        page_obj = paginator.page(page_number)
+    except (PageNotAnInteger, EmptyPage):
+        raise Http404('No such page exists.')
 
     return render(
         request,
@@ -29,9 +26,10 @@ def item_list(request: HttpRequest) -> HttpResponse:
 
 def item_detail(request: HttpRequest, product_id: int) -> HttpResponse:
     """View for the item detail."""
-    product = CatalogItem.objects.get(
-        id=product_id,
-    )
+    product = item.get_detail_by(pk=product_id)
+
+    if not product:
+        raise Http404('No such page exists.')
 
     return render(
         request,
