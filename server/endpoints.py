3f916db76b31f6b523145826e5465a1135dfa26a
MrEndor <mrendorer@gmail.com>
2023-03-21 18:34:49 +0300

    feat(coffee): change coffee endpoint

--- server/endpoints.py
+++ server/endpoints.py
@@ -8,6 +8,11 @@ COFFEE_TEXT: Final[str] = 'Я чайник'
 
 def coffee(request: HttpRequest) -> HttpResponse:
     """Im a teapot."""
+    if request.user.is_authenticated:
+        profile = request.user.profile
+        profile.coffee_count += 1
+        profile.save()
+
     return HttpResponse(
         '<body>{text}</body>'.format(text=COFFEE_TEXT),
         status=HTTPStatus.IM_A_TEAPOT,
